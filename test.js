e61f75c758015d08a480aa179d18dd61b8fb3f2e
Fuji, Goro <g.psy.va@gmail.com>
2012-01-10 00:07:21 +0900

    Loading entry point

--- test.js
+++ test.js
@@ -6,6 +6,4 @@ var Clion = require('./');
 var file = process.argv[2];
 var img = new Clion.Image(file);
 
-//console.log('image_info:', img.image_info);
-//console.log('image:', img);
-img.dis_type();
+img.dump();
