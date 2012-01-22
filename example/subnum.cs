687977940f48c785dcf7f187d85a53550a909341
Fuji, Goro <g.psy.va@gmail.com>
2012-01-22 17:54:00 +0900

    Execute "hello, world!"

--- example/subnum.cs
+++ example/subnum.cs
@@ -2,7 +2,7 @@ class HelloWorld {
     static void Main() {
         var x = 40;
         var y = 2;
-        var result = x + y;
+        var result = x - y;
         System.Console.WriteLine(result);
     }
 }
